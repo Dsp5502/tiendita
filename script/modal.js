d1bb2bd75a27cb914a5b5e035c43e7ca55a24f92
David Puerto Guerrero <dsp5502@gmail.com>
2022-03-15 09:17:15 -0500

    moda seleccionar productos

--- script/modal.js
+++ script/modal.js
@@ -1,8 +1,8 @@
 if (document.getElementById('btnModal')) {
-  var modal = document.getElementById('myModal');
-  var btn = document.getElementById('btnModal');
-  var span = document.getElementsByClassName('close')[0];
-  var body = document.getElementsByTagName('body')[0];
+  let modal = document.getElementById('myModal');
+  let btn = document.getElementById('btnModal');
+  let cerrar = document.getElementsByClassName('close')[0];
+  let body = document.getElementsByTagName('body')[0];
 
   btn.onclick = function () {
     modal.style.display = 'block';
@@ -12,8 +12,42 @@ if (document.getElementById('btnModal')) {
     body.style.overflow = 'hidden';
   };
 
-  span.onclick = function () {
+  cerrar.onclick = function () {
     modal.style.display = 'none';
+
+    body.style.position = 'inherit';
+    body.style.height = 'auto';
+    body.style.overflow = 'visible';
+  };
+
+  window.onclick = function (event) {
+    if (event.target == modal) {
+      modal.style.display = 'none';
+
+      body.style.position = 'inherit';
+      body.style.height = 'auto';
+      body.style.overflow = 'visible';
+    }
+  };
+}
+
+// ----------------------------------------------------------------
+if (document.getElementById('btnModal1')) {
+  var modal1 = document.getElementById('myModal1');
+  var btn1 = document.getElementById('btnModal1');
+  var span1 = document.getElementsByClassName('close')[1];
+  var body = document.getElementsByTagName('body')[1];
+
+  btn1.onclick = function () {
+    modal1.style.display = 'block';
+
+    body.style.position = 'static';
+    body.style.height = '100%';
+    body.style.overflow = 'hidden';
+  };
+
+  span1.onclick = function () {
+    modal1.style.display = 'none';
 
     body.style.position = 'inherit';
     body.style.height = 'auto';

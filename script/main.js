8528099ae849bc36b59a436d851a888ac2918184
David Puerto Guerrero <dsp5502@gmail.com>
2022-03-16 22:36:22 -0500

    pasarela de pagos comenzada

--- script/main.js
+++ script/main.js
@@ -2,69 +2,101 @@ import getData from './getData.js';
 import showData from './showdata.js';
 import './modal.js';
 import modalProductos from './modalProducto.js';
-import showCarrito from './showCarrito.js';
+// import showCarrito from './showCarrito.js';
 
 let carrito = [];
-const DOMcarrito = document.querySelector('#carrito');
 
 const urlProductos = 'https://api-prueba-productos.herokuapp.com/productos';
 
 const cardsProductsOfer = document.getElementById('ContentCards');
-console.log(cardsProductsOfer);
 
+const numeroCarrito = document.getElementById('numeroCarrito');
 const numeroProductos = document.getElementById('numeroProductos');
 numeroProductos.innerText = `${carrito.length}`;
-console.log(numeroProductos);
+numeroCarrito.innerText = `${carrito.length}`;
 
 const btnProducto = document.getElementById('ContentCards');
 const modalHeader = document.getElementById('modal-content');
 const productosCarrito = document.getElementById('productosCarrito');
-console.log(productosCarrito);
 
 document.addEventListener('DOMContentLoaded', async () => {
   const datosProd = await getData(urlProductos);
-  console.log(datosProd);
   showData(datosProd, cardsProductsOfer);
+  getLocalStorage();
+  sumar();
 });
 
 btnProducto.addEventListener('click', async (e) => {
   const id = e.target.id;
-  console.log({ id });
   const mostrarProducto = e.target.classList.contains('btnModal1');
   if (mostrarProducto) {
     const listaProductos = await getData(urlProductos);
-
     const productoSelec = listaProductos.find((p) => p.id == Number(id));
-    console.log(productoSelec);
     modalProductos(productoSelec, modalHeader);
-
     const btnModal1 = document.querySelector('.addCart');
-    console.log(btnModal1);
     btnModal1.addEventListener('click', async (e) => {
-      carrito.push(productoSelec);
-      console.log('hola');
-      console.log(carrito);
-      showCarrito(carrito, productosCarrito);
       const numeroProductos = document.getElementById('numeroProductos');
+
       numeroProductos.innerText = `${carrito.length}`;
-      console.log(numeroProductos);
+      numeroCarrito.innerText = `${carrito.length}`;
+      carrito.push(productoSelec);
+      localStorage.setItem('productos', JSON.stringify(carrito));
+      // showCarrito(carrito, productosCarrito);
+      getLocalStorage();
+      sumar();
     });
   }
 });
 
-// const renderizarCarrito = async () => {
-//   productosCarrito.textContent = '';
-//   const datosProd = await getData(urlProductos);
-//   const carritoSinDuplicados = [...new Set(carrito)];
-//   carritoSinDuplicados.forEach((producto) => {
-//     const miProducto = datosProd.filter((productoBD) => {
-//       return productoBD.id === Number(producto);
-//     });
-//     const unidadeItem = carrito.reduce((total, itemid) => {
-//       return itemid === producto ? (total += 1) : total;
-//     });
-//     productosCarrito.textContent = `${unidadeItem} x ${miProducto.nombre} - ${miProducto.precio}`;
-//   });
-// };
+const vaciarCanasta = document.getElementById('vaciarCanasta');
+vaciarCanasta.addEventListener('click', () => {
+  carrito = [];
+  numeroProductos.innerText = `${carrito.length}`;
+  numeroCarrito.innerText = `${carrito.length}`;
+  // showCarrito(carrito, productosCarrito);
+  getLocalStorage();
+  sumar();
+  localStorage.clear();
+  window.location.reload();
+});
+
+const getLocalStorage = () => {
+  let carrito1 = JSON.parse(localStorage.getItem('productos'));
+  console.log(carrito1);
+  const prueba = document.getElementById('idFooterCart');
+  console.log(prueba);
+
+  numeroProductos.innerText = `${carrito1.length}`;
+  numeroCarrito.innerText = `${carrito1.length}`;
+  productosCarrito.innerHTML = '';
+  carrito1.forEach((element) => {
+    const { nombre, precio, imagen, id } = element;
+    productosCarrito.innerHTML += `
+      <div class="productListadoCarrito">
+                <img src=${imagen} alt="imagen" />
+                <div class="especifiProduct">
+                  <span class="nombreselectprod">${nombre}</span>
+                  <p class="precioselectprod"><strong>${precio}</strong></p>
+                  <div>
+                  <span  id=${id} class="pesoprecio"><i> -  </i>  250g  <i>  + </i></span>
+                  </div>
+                </div>
+              </div>
+      
+      `;
+  });
+};
+
+const sumar = () => {
+  let carritoSuma = JSON.parse(localStorage.getItem('productos'));
+  let suma = 0;
+  carritoSuma.forEach((element) => {
+    const { precio } = element;
+    const precioNuevo = Math.round(Number(precio.replace('$', '')));
+    suma += precioNuevo;
+    console.log(suma);
+  });
+  document.getElementById('total').innerHTML = `$${suma}`;
+};
 
 console.log({ carrito });
--- script/showCarrito.js
+++ script/showCarrito.js
@@ -1,30 +1,30 @@
-const showCarrito = async (arrProducts, card) => {
-  card.innerHTML = '';
+// const showCarrito = async (arrProducts, card) => {
+//   card.innerHTML = '';
 
-  let productosCarrito = await arrProducts;
-  const carritoSinduplicados = [...new Set(productosCarrito)];
+//   let productosCarrito = await arrProducts;
+//   const carritoSinduplicados = [...new Set(productosCarrito)];
 
-  productosCarrito.forEach((producto) => {
-    const { nombre, precio, imagen } = producto;
-    card.innerHTML += `
-    <div class="productListadoCarrito">
-              <img src=${imagen} alt="imagen" />
-              <div class="especifiProduct">
-                <span class="nombreselectprod">${nombre}</span>
-                <p class="precioselectprod"><strong>${precio}</strong></p>
-              </div>
-            </div>
-      `;
-  });
-};
+//   productosCarrito.forEach((producto) => {
+//     const { nombre, precio, imagen } = producto;
+//     card.innerHTML += `
+//     <div class="productListadoCarrito">
+//               <img src=${imagen} alt="imagen" />
+//               <div class="especifiProduct">
+//                 <span class="nombreselectprod">${nombre}</span>
+//                 <p class="precioselectprod"><strong>${precio}</strong></p>
+//               </div>
+//             </div>
+//       `;
+//   });
+// };
 
-// carritoSinduplicados.forEach((product) => {
-//   const { nombre, imagen } = product;
-//   card.innerHTML += `
-//   <div>${nombre}
-//   <img src=${imagen} alt="">
-//   </div>
-//   `;
-// });
+// // carritoSinduplicados.forEach((product) => {
+// //   const { nombre, imagen } = product;
+// //   card.innerHTML += `
+// //   <div>${nombre}
+// //   <img src=${imagen} alt="">
+// //   </div>
+// //   `;
+// // });
 
-export default showCarrito;
+// export default showCarrito;
